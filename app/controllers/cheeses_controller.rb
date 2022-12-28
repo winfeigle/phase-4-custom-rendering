727d613a8786e97add907762e78a3c3253927173
Win Feigle <win.feigle@gmail.com>
2022-12-28 10:05:30 -0700

    commit tree 2ff69b717098d218bc43a5f4afb94575d251d2ab

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,18 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: {error: 'Cheese not found'}, status: :not_found
+    end
   end
 
 end
